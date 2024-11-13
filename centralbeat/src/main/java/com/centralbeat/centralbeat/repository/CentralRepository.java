68f59497caee800c177080a2b35522c3a868dd3c
Thiago-234 <camposthiago2021@gmail.com>
2024-11-13 15:33:51 -0300

    Delete centralbeat directory

--- centralbeat/src/main/java/com/centralbeat/centralbeat/repository/CentralRepository.java
+++
@@ -1,9 +0,0 @@
-package com.centralbeat.centralbeat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.centralbeat.centralbeat.model.CentralModel;
-
-public interface CentralRepository extends JpaRepository<CentralModel, Long>{
-	
-}
